97304d68607f965f5a82c480bbc83cb6cd086adc
s0600204 <dev@s0600204>
2026-04-12 22:14:34 +0100

    refactor: (Re)implement DanteMulticastService

--- netaudio/dante3/service.py
+++ netaudio/dante3/service.py
@@ -1,6 +1,11 @@
 import asyncio
 from enum import Enum
+from ipaddress import IPv4Address
+import socket
+import struct
 from typing import TYPE_CHECKING
+
+import ifaddr
 
 from .util.consts import LOGGER
 from .util.helpers import bytes2int
@@ -48,6 +53,68 @@ class DanteDiscoverableService:
     @classmethod
     def build_service_descriptor(cls, mdns_service_info: MDNSServiceInfo) -> None:
         raise NotImplementedError
+
+
+class DanteMulticastProtocol(asyncio.DatagramProtocol):
+
+    def __init__(self, callback: callable):
+        self._callback: callable = callback
+        self._transport: asyncio.DatagramTransport | None = None
+
+    def connection_made(self, transport: asyncio.DatagramTransport) -> None:
+        self._transport = transport
+
+    def datagram_received(
+        self,
+        data: bytes,
+        addr: tuple[str, int]
+    ) -> None:
+        self._callback(IPv4Address(addr[0]), data)
+
+
+class DanteMulticastService(_DanteService):
+
+    SERVICE_MCAST_GRP: str | None = None
+
+    _ignored_addrs: list[str] = ['127.0.0.1']
+
+    def __init__(self, application: DanteApplication):
+        super().__init__(application)
+        self._protocol: asyncio.DatagramProtocol | None = None
+        self._transport: asyncio.DatagramTransport | None = None
+
+    def _build_socket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("0.0.0.0", self.SERVICE_PORT))
+
+        for adapter in ifaddr.get_adapters():
+            for addr in adapter.ips:
+                if not addr.is_IPv4 or addr.ip in self._ignored_addrs:
+                    LOGGER.debug("Not binding to %s on %s", addr.ip, addr.nice_name)
+                    continue
+                mreq = struct.pack("4s4s", socket.inet_aton(self.SERVICE_MCAST_GRP), socket.inet_aton(addr.ip))
+                sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+        return sock
+
+    def _receive(
+        self,
+        ipv4_addr: IPv4Address,
+        message: bytes,
+    ) -> None:
+        raise NotImplementedError
+
+    async def start(self):
+        self._transport, self._protocol = await self._app.event_loop.create_datagram_endpoint(
+            lambda: DanteMulticastProtocol(self._receive),
+            sock=self._build_socket(),
+        )
+
+    async def stop(self):
+        if self._transport:
+            self._transport.close()
+            self._transport = None
 
 
 class DanteUnicastProtocol(asyncio.DatagramProtocol):
